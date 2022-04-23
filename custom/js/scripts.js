383fde923211f437ad6a346ef590d61989d5c634
Hawon Park <hawonp@gmail.com>
2022-04-23 13:25:20 +0900

    [WIP] Portfolio Work Experience

--- custom/js/scripts.js
+++ custom/js/scripts.js
@@ -1,1 +1,13 @@
-mdc.ripple.MDCRipple.attachTo(document.querySelector(".foo-button"));
+// mdc.ripple.MDCRipple.attachTo(document.querySelector(".foo-button"));
+
+$(document).ready(function () {
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
+function openNewTab(link) {
+  window.open(link, "_blank");
+}
