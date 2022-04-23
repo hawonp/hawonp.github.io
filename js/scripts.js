de65432e15be0d51656c3cf9a5613b3db1afaee6
Hawon Park <hawonp@gmail.com>
2022-04-23 15:22:08 +0900

    Added navbar

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,23 @@ $(document).ready(function () {
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
+
+  $(this).scrollTop(0);
+  var activeNavItem = $(".nav-item");
+
+  activeNavItem.click(function () {
+    activeNavItem.removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $("#see_more_btn").addEventListener("click", function () {
+    // var activeNavItem = $(".nav-item");
+    // activeNavItem.removeClass("active");
+    // $("#about_me").addClass("active");
+  });
 });
 function openNewTab(link) {
   window.open(link, "_blank");
 }
+
+// <!-- onclick="document.getElementById('about_me').scrollIntoView();" -->
